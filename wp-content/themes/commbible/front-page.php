9cdb130e3539ffea892d3367496a8afa423f1230
bozdoz <howaboutben@gmail.com>
2021-05-02 13:46:47 -0300

    second section needs to be bumped above previous section loop

--- wp-content/themes/commbible/front-page.php
+++ wp-content/themes/commbible/front-page.php
@@ -69,7 +69,7 @@ get_header(); ?>
 			$num_sections = apply_filters( 'twentyseventeen_front_page_sections', 4 );
 
 			// Create a setting and control for each of the sections available in the theme.
-			for ( $i = $before_home; $i < ( 1 + $num_sections ); $i++ ) {
+			for ( $i = $before_home + 1; $i < ( 1 + $num_sections ); $i++ ) {
 				$twentyseventeencounter = $i;
 				twentyseventeen_front_page_section( null, $i );
 			}
